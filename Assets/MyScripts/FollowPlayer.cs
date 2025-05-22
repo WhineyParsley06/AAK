3e309fb21144df19a71253cf010349bc3440f9ae
David Valderrama <david.valderrama@utp.edu.co>
2025-05-22 13:12:45 -0500

    Commit 1:12 p.m.

---
+++ Assets/MyScripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+    public GameObject player;
+    public float x = 0;
+    public float y = 3;
+    public float z = -6;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = player.transform.position + new Vector3(x, y, z);
+        transform.LookAt(player.transform);
+    }
+}
